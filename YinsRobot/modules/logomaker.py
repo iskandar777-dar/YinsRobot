1b2a8c7115ea79f34f0e3a7acc7c685b3a27bdd9
iskandar777-dar <54578330+iskandar777-dar@users.noreply.github.com>
2022-10-03 18:38:13 +0700

    Update logomaker.py

--- YinsRobot/modules/logomaker.py
+++ YinsRobot/modules/logomaker.py
@@ -113,7 +113,7 @@ async def logo_gen(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Yins Robot](https://t.me/YinzRobot)",
+            caption="Logo by [Somed]",
             force_document=False,
         )
         os.remove(flnme)
@@ -196,7 +196,7 @@ async def logo_(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Yins Robot](https://t.me/YinzRobot)",
+            caption="Logo by [Somed]",
             force_document=False,
         )
         os.remove(flnme)
